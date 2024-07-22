2fbbe9fd96db2324a393402a353159b5ece35cd5
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-22 21:43:03 +0100

    feat: add webpack configuration file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,33 @@
+import $ from "jquery";
+import _ from "lodash";
+
+let count = 0;
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+let debounceUpdateCounter = _.debounce(updateCounter, 500, { leading: true });
+
+$(function() {
+  const textsArr = [
+    "Holberton Dashboard",
+    "Dashboard data for the students",
+    "Click here to get started",
+    "<p id='count'></p>",
+    "Copyright - Holberton School",
+  ]
+
+  for (let i= 0; i < 2; i++) {
+    $("body").append("<p>" + textsArr[i] + "</p>");
+  }
+
+  $("body").append("<button>" + textsArr[2] + "</button>");
+
+  $("body").append("<p id='count'> " + textsArr[3] + "</p>");
+  $("body").append("<p>" + textsArr[4] + "</p>");
+
+  $("button").on("click", debounceUpdateCounter);
+
+});
+

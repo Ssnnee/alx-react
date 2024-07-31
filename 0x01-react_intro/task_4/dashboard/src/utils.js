63c3ae715d27d4bc295c9ae29eebf37ec128aad1
Samuel Nandi <nandisamuelsne@gmail.com>
2024-08-01 00:08:02 +0100

    Deploy one

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,16 @@
+function getFullYear() {
+  return new Date().getFullYear();
+}
+
+function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
+}
+
+function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };

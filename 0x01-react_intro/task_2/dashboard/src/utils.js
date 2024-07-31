b33eef975e9a5565cb0b858daabef93a92ffb731
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-31 23:30:39 +0100

    Modify Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -9,4 +9,8 @@ function getFooterCopy(isIndex) {
   return 'Holberton School main dashboard';
 }
 
-export { getFullYear, getFooterCopy };
+function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };

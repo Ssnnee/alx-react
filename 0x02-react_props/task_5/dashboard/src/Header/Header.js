9955efaf9249263f0604aef6e4d8f4f561cdb150
Samuel Nandi <nandisamuelsne@gmail.com>
2024-08-01 12:38:00 +0100

    Create a Header component

---
+++ 0x02-react_props/task_5/dashboard/src/Header/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+      School dashboard
+      </h1>
+    </header>
+  );
+};

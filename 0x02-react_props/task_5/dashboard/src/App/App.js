4e29d37248efc6088a27bc4ce40bf953cefda6d4
Samuel Nandi <nandisamuelsne@gmail.com>
2024-08-01 12:55:09 +0100

    Style components

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "../assets/holberton-logo.jpg";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
 
 function App() {
   return (

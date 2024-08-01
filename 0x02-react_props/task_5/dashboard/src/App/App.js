a75ce182821f64235dc6862a2cd465a6cdc4a1e0
Samuel Nandi <nandisamuelsne@gmail.com>
2024-08-01 12:46:44 +0100

    Create a Login component

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -9,13 +9,6 @@ function App() {
 
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' />
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' />
-          <button type='submit'>OK</button>
-        </form>
       </div>
 
     </div>
---
+++ 0x02-react_props/task_5/dashboard/src/Login/Login.js
@@ -0,0 +1,13 @@
+export const Login = () => {
+  return (
+    <>
+      <form>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='submit'>OK</button>
+      </form>
+    </>
+  );
+};
